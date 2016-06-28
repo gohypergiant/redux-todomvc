18ba20b33b7f401525a481499e071e8e72b4361e
Brandon Pierce <brandon@brandonjpierce.com>
2016-06-28 11:53:05 -0400

    Release prep

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.listen(port, error => {
   if (error) {
     console.error(error);
   } else {
-    console.info("==> 🌎Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port);
+    console.info('==> 🌎Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port);
   }
 });
